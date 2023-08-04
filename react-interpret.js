fd7c4862b70e72c87fc4b146e61d78c28e070453
Arsen Shkrumelyak <arsen@nerverift.com>
2023-08-04 14:19:12 +0100

    Initial commit

---
+++ react-interpret.js
@@ -0,0 +1,26 @@
+// react-interpret.js
+
+/*
+text: string (text to be interpreted)
+interpreters: object (key: string, value: string, function, or component)
+*/
+function interpret(text, interpreters) {
+    const regex = /\/?(.*?)/g;
+    let parts = text.split(regex);
+    let inTag = false;
+
+    parts = parts.map((part, index) => {
+        if (interpreters[part] !== undefined) {
+            inTag = !inTag;
+            return interpreters[part];
+        } else if (inTag) {
+            return part;
+        } else {
+            return part;
+        }
+    });
+
+    return parts;
+}
+
+export { interpret };
